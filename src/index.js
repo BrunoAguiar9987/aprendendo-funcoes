ab5b5d6c64a560a4c7e8e5eb9266e8216b1af0b3
BrunoAguiar9987 <bruno.olliveira9987@gmail.com>
2025-05-04 15:21:38 -0300

    Primeiro exemplo de função

---
+++ src/index.js
@@ -0,0 +1,20 @@
+function cadastro() {
+   console.log("escreva seu nome")
+   if(contemSobrenome = true) {
+      console.log("Agora esvrea seu sobrenome!")
+   }
+}
+
+function salvandoCadastro() {
+   console.log("salvo como sucesso no banco de dados")
+}
+
+function exitMessage() {
+   console.log(" sair do programa!")
+}
+
+function main() {
+   cadastro()
+   salvandoCadastro()
+   exitMessage()
+}
