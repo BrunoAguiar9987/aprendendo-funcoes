9afe4c340eb4bb2808d95f0eae09a1c96bf88542
BrunoAguiar9987 <bruno.olliveira9987@gmail.com>
2025-05-04 15:32:41 -0300

    Outro exemplo de funcçao

---
+++ src/exFuncao.js
@@ -0,0 +1,12 @@
+
+function enviarDados() {
+   let noomeDoBanco = "banco-de-dados"
+   console.log("Salvando dados em : " + noomeDoBanco)
+   //aqui pode conter uma infinidade de logicas diferentes
+
+   if (3 === 3){
+       console.log("Senha Validada")
+   }
+}
+
+enviarDados()
